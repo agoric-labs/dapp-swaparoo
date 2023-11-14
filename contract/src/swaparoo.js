0ea1cd93aede889a17299155651c5c6e5ac85531
Jim Larson <jim@agoric.com>
2023-11-13 17:20:56 -0800

    feat: check proposal shapes

--- contract/src/swaparoo.js
+++ contract/src/swaparoo.js
@@ -7,7 +7,7 @@ import { AmountMath, AssetKind } from '@agoric/ertp/src/amountMath.js';
 import { M, getCopyBagEntries } from '@endo/patterns';
 import { E, Far } from '@endo/far';
 import '@agoric/zoe/exported.js';
-import { swap } from '@agoric/zoe/contractSupport/zoeHelpers.js';
+import { atomicRearrange } from '@agoric/zoe/contractSupport/zoeHelpers.js';
 
 import { makeScalarBigMapStore } from '@agoric/vat-data';
 
@@ -16,6 +16,29 @@ import { makeTracer } from './debug.js';
 const { Fail, quote: q } = assert;
 
 const trace = makeTracer('Swaparoo', true);
+
+/** @type {Swap} */
+export const swapWithFee = (zcf, firstSeat, secondSeat, feeSeat, feeAmount) => {
+  try {
+    const { Fee: _, ...firstGive } = firstSeat.getProposal().give;
+    atomicRearrange(zcf,
+      harden([
+        [firstSeat, secondSeat, firstGive]
+        [secondSeat, firstSeat, secondSeat.getProposal().give],
+        [firstSeat, feeSeat, { Fee: feeAmount }],
+      ]),
+    );
+  } catch (err) {
+    firstSeat.fail(err);
+    secondSeat.fail(err);
+    throw err;
+  }
+
+  firstSeat.exit();
+  secondSeat.exit();
+  return defaultAcceptanceMsg;
+};
+
 
 /**
  * @param {ZCF<{joinPrice: Amount}>} zcf
@@ -46,15 +69,17 @@ export const start = async zcf => {
       };
     };
 
-    const makeSecondProposalShape = (give, want) => {
-      return M.any(); // XXX do better
+    const makeSecondProposalShape = want => {
+      return M.splitRecord({
+        give: M.splitRecord(Object.fromEntries(new Map(Object.keys(want).map(k => [k])))),
+      });
     };
 
     const secondSeatInvitation = zcf.makeInvitation(
       secondSeatOfferHandler,
       'matchOffer',
       { give, want }, // "give" and "want" are from the proposer's perspective
-      makeSecondProposalShape(give, want),
+      makeSecondProposalShape(want),
     );
 
     return secondSeatInvitation;
@@ -64,7 +89,7 @@ export const start = async zcf => {
   const makeFirstInvitation = (issuers) => {
     issuers.forEach(i => zcf.addIssuer(i)); // TODO cleanup
     const proposalShape = M.splitRecord({
-      give: M.splitRecord({ fee: feeShape }),
+      give: M.splitRecord({ Fee: feeShape }),
     });
     return zcf.makeInvitation(makeSecondInvitation, 'create a swap', proposalShape);
   };
